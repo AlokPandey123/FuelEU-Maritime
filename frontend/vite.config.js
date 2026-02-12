caf6cd362c165ed4cc47c13eaea385b7cdfeaf94
KumarVK <alokpandey6386561331@gmail.com>
2026-02-13 00:30:17 +0530

    remove proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/routes': 'https://fueleu-maritime-ogc0.onrender.com/',
-      '/compliance': 'https://fueleu-maritime-ogc0.onrender.com/',
-      '/banking': 'https://fueleu-maritime-ogc0.onrender.com/',
-      '/pools': 'https://fueleu-maritime-ogc0.onrender.com/',
-    },
-  },
 })
